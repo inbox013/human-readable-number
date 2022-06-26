7c133bd89e2f373580d205ca817aa85c649fdd19
inbox013 <inbox0130@gmail.com>
2022-06-26 09:50:29 +0300

    init: add if annswer.length ===3

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ module.exports = function toReadable (number) {
    let tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
    if (answer.length === 1) {
       return from0to9[answer];
-   };
-   if (answer.length === 2) {
+   }
+   else if (answer.length === 2) {
       if (answer[0] == 1) {
          return from10to19[answer[1]];
       } if (answer[1] == 0) {
@@ -14,5 +14,18 @@ module.exports = function toReadable (number) {
       } if (answer[1] > 0 && answer[1] < 10) {
          return tens[answer[0]] + " " + from0to9[answer[1]];
       };
+   }
+   else if (answer.length === 3) {
+      if (answer[1] == 0 & answer[2] == 0) {
+         return from0to9[answer[0]] + " hundred";
+      }  else if (answer[1] == 1) {
+         return from0to9[answer[0]] + " hundred " + from10to19[answer[2]];
+      } else if (answer[1] != 0 && answer[2] == 0) {
+         return from0to9[answer[0]] + " hundred " + tens[answer[1]];
+      } else if (answer[1] == 0 && answer[2] != 0) {
+         return from0to9[answer[0]] + " hundred " + from0to9[answer[2]];
+      } else if (answer[1] !=0 && answer[2] !=0) {
+         return from0to9[answer[0]] + " hundred " + tens[answer[1]] + " " + from0to9[answer[2]];
+      }
    };
-}
+};
